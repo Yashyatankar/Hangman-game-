3d1bda8c3dae260411c9e6249cd19c1cb2730263
Yash <yashrpatankar24@gmail.com>
2025-11-01 21:36:20 +0530

    Create Hangman.py

---
+++ Hangman.py
@@ -0,0 +1,19 @@
+import random
+
+lives = 7
+is_running = True
+
+
+words = ['python', 'developer', 'keyboard', 'hangman', 'programming']
+
+def hit():
+    pass
+
+def main():
+    
+    anwser = random.choice(words)
+    print(anwser)
+
+if __name__ == '__main__':
+    main()
+
